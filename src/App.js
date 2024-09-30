3d0e9cab4346af4c77d8119651327678cbc0e3cb
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-09-29 19:21:30 -0500

    init

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
       <div className="App">
           <Header/>
           <Routes>
-              <Route index element={<ProductList />}></Route>
-              <Route path={'form'} element={<Form />}></Route>
+              <Route index element={<ProductList />} />
+              <Route path={'form'} element={<Form />} />
           </Routes>
       </div>
   );
