03a5529fd656d0466a3e3de4270211c44c4b65b5
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-10-09 11:40:57 -0500

    test commit

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Add useState here
+import React, {useCallback, useEffect, useState} from 'react'; // Add useState here
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
@@ -22,6 +22,27 @@ const getTotalPrice = (items) => {
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const { tg } = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+        }
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, []);
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData]);
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id); // Fix the typo
