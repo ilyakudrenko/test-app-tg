1bbd46ffc9830ca903e7c2c296520e3574f46b08
ilyakudrenko <ilya.kudrenko@gmail.com>
2024-10-25 03:01:14 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,141 +1,27 @@
-import '@telegram-apps/telegram-ui/dist/styles.css';
+import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
-import {AppRoot, Placeholder, Button, Card, } from '@telegram-apps/telegram-ui';
-import {CardChip} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip";
-import {CardCell} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell";
-import React from "react";
-import {
-    HorizontalScroll
-} from "@telegram-apps/telegram-ui/dist/components/Service/HorizontalScroll/HorizontalScroll";
+function App() {
+    const {tg, onToggleButton} = useTelegram();
 
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-const App = () => (
-    <AppRoot>
-        <Placeholder
-            header="Title"
-            description="App.js test"
-        >
-            <img
-                alt="Telegram sticker"
-                src="https://xelene.me/telegram.gif"
-                style={{ display: 'block', width: '144px', height: '144px' }}
-            />
-            <Button
-                mode="bezeled"
-                size="m"
-            >
-                Action
-            </Button>
-
-            <HorizontalScroll>
-                <div style={{display: 'flex', overflowX: 'auto', gap: '16px'}}>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot place
-                            </CardChip>
-                            <img
-                                alt="Dog"
-                                src="https://i.imgur.com/892vhef.jpeg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="United states"
-                            >
-                                New York
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                </div>
-            </HorizontalScroll>
-
-        </Placeholder>
-    </AppRoot>
-);
+  return (
+      <div className="App">
+          <Header/>
+          <Routes>
+              <Route index element={<ProductList />}></Route>
+              <Route path={'form'} element={<Form />}></Route>
+          </Routes>
+      </div>
+  );
+}
 
 export default App;
-
-
-// import './App.css';
-// import { useEffect } from "react";
-//
-// function App() {
-//     const tg = window.Telegram ? window.Telegram.WebApp : null;
-//
-//     useEffect(() => {
-//         if (tg) {
-//             tg.ready();
-//             console.log("Telegram WebApp is ready");
-//         } else {
-//             console.log("Telegram WebApp is not available");
-//         }
-//     }, [tg]);
-//
-//
-//     const onClose = () => {
-//         if (tg) {
-//             tg.close();
-//         }
-//     }
-//
-//     return (
-//         <div className="App">
-//             Hello World
-//             <button className={'button'} onClick={onClose}>Закрыть</button>
-//         </div>
-//     );
-// }
-//
-// export default App;
